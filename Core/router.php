8fc24d4052869f4ce85ee34c597409f43fe80c92
JayRhuuu <jayrhuelplaton22@gmail.com>
2025-09-03 23:47:39 +0800

    Automatically Redirect Back Upon Failed Validation

--- Core/router.php
+++ Core/router.php
@@ -65,6 +65,12 @@ class Router
         }
         $this->abort();
     }
+
+    public function previousUrl()
+    {
+        return redirect($_SERVER['HTTP_REFERER']);
+    }
+
     protected function abort($code = 404)
     {
         http_response_code($code);
--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Session;
+use Core\ValidationException;
 
 session_start();
 
@@ -9,9 +10,9 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . 'Core/functions.php';
 
 spl_autoload_register(function ($class) {
-    
+
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    
+
     require base_path("{$class}.php");
 });
 
@@ -23,6 +24,13 @@ $routes = require base_path('routes.php');
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
-$router->route($uri, $method);
+try {
+    $router->route($uri, $method);
+} catch (ValidationException $exception) {
+    Session::flash('errors', $exception->errors);
+    Session::flash('old', $exception->old);
+
+    return redirect($router->previousUrl());
+}
 
 Session::unflash();
